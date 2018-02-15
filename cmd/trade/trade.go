f9105f5d2cb0715b3f661225847445c738537f39
Alex Sugak <aleksandr.sugak@gmail.com>
2018-02-15 14:48:34 +0200

    add basic http api structure

---
+++ cmd/trade/trade.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	prefixed "github.com/gz-c/logrus-prefixed-formatter"
+	"github.com/sirupsen/logrus"
+	"github.com/skycoin/getsky-trade/src/trade"
+)
+
+func main() {
+	log := initLogger()
+	server := trade.NewHTTPServer(log)
+	defer server.Shutdown()
+	if err := server.Run(); err != nil {
+		panic(err.Error())
+	}
+}
+
+func initLogger() logrus.FieldLogger {
+	log := logrus.New()
+	log.Out = os.Stdout
+	log.Formatter = &prefixed.TextFormatter{
+		FullTimestamp:      true,
+		AlwaysQuoteStrings: true,
+		QuoteEmptyFields:   true,
+		ForceFormatting:    true,
+	}
+
+	return log.WithField("prefix", "trade")
+}
---
+++ src/trade/http.go
@@ -0,0 +1,106 @@
+package trade
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	errInternalServerError = errors.New("Internal Server Error")
+)
+
+type HTTPServer struct {
+	log          logrus.FieldLogger
+	httpListener *http.Server
+	quit         chan os.Signal
+	done         chan struct{}
+}
+
+func NewHTTPServer(log logrus.FieldLogger) *HTTPServer {
+	return &HTTPServer{
+		log: log.WithFields(logrus.Fields{
+			"prefix": "trade.http",
+		}),
+		quit: make(chan os.Signal, 1),
+		done: make(chan struct{}),
+	}
+}
+
+func (s *HTTPServer) Run() error {
+	log := s.log
+	log.Info("HTTP service start")
+	defer log.Info("HTTP service stop")
+	signal.Notify(s.quit, os.Interrupt)
+
+	r := s.setupRouter()
+
+	s.httpListener = &http.Server{
+		Addr:         "0.0.0.0:8081",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 15,
+		Handler:      r,
+	}
+
+	errorC := make(chan error)
+	go func() {
+		if err := s.httpListener.ListenAndServe(); err != nil {
+			log.Error(err)
+			errorC <- err
+		}
+	}()
+
+	select {
+	case err := <-errorC:
+		return err
+	case <-s.quit:
+		return nil
+	}
+}
+
+func (s *HTTPServer) Shutdown() {
+	s.log.Info("HTTP service shutting down")
+	close(s.done)
+
+	// Create a deadline to wait for.
+	wait := time.Second * 15
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	s.httpListener.Shutdown(ctx)
+}
+
+func (s *HTTPServer) setupRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api", APIInfoHandler(s)).Methods("GET")
+
+	return r
+}
+
+type APIInfoResponse struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Version     int    `json:"version"`
+}
+
+func APIInfoHandler(s *HTTPServer) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		info := APIInfoResponse{
+			Name:        "trade API",
+			Description: "trade API provides endpoints to enable posting and searching Skycoin buy and sell adverts",
+			Version:     1,
+		}
+
+		json.NewEncoder(w).Encode(info)
+	}
+}
