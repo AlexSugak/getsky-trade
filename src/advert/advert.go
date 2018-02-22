867dabfe022d7170bd270426a4d63c2d7a684464
Alex Sugak <aleksandr.sugak@gmail.com>
2018-02-22 15:52:44 +0200

    fix lint errors

--- src/advert/advert.go
+++ src/advert/advert.go
@@ -4,16 +4,25 @@ import (
 	"time"
 )
 
+// TradeOption represents different types of trade options
 type TradeOption int
-type AdvertType int
+
+// Type represents an Advert type (e.g. sell or buy)
+type Type int
 
 const (
+	// CashInPerson trade option stands for cache exchange happening in person
 	CashInPerson TradeOption = iota + 1
+	// CashByMail trade option stands for cache exchange happening via mail
 	CashByMail
+	// MoneyOrderByMail trade option stands for cache exchange happening via money order sent by mail
 	MoneyOrderByMail
+	// Other unknown trade option
 	Other
 
-	Sell AdvertType = iota + 1
+	// Sell advert type
+	Sell Type = iota + 1
+	// Buy advert type
 	Buy
 )
 
@@ -22,7 +31,7 @@ type Advert struct {
 	ID           int
 	Expires      time.Time
 	TradeOptions []TradeOption
-	Type         AdvertType
+	Type         Type
 	Amount       uint64
 	Location     string
 	Seller       string
