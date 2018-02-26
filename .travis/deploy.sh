d640acd6662bebb7bd0127f4465ff03f6d2cecc5
Alex Sugak <aleksandr.sugak@gmail.com>
2018-02-26 16:20:16 +0200

    add deploy server to known hosts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,8 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
+ssh-keyscan $IP >> ~/.ssh/known_hosts
+
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
