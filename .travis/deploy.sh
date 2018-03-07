afada1db2a28c38bbe314a4b3191a7464e34b9a7
Alex Sugak <aleksandr.sugak@gmail.com>
2018-03-07 12:01:41 +0200

    update deploy keys

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,28 +3,28 @@ set -e # exit on first error
 
 eval "$(ssh-agent -s)" # start ssh-agent cache
 # id_rsa is decrypted as the first step of Travis build, see .travis.yml
-chmod 600 .travis/id_rsa # allow read access to the private key
-ssh-add .travis/id_rsa # add the private key to SSH
+chmod 600 .travis/id_rsa.getsky.deploy # allow read access to the private key
+ssh-add .travis/id_rsa.getsky.deploy # add the private key to SSH
 
 # prevent authenticity confirmations 
 ssh-keyscan $IP >> ~/.ssh/known_hosts
 
 # shut down any currently running containers to prevent file locking when pushing new version
-ssh apps@$IP -p $PORT sudo -iu apps <<EOF
+ssh $RUN_USER@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  sudo docker-compose kill
+  docker-compose kill
 EOF
 
 # push latest changes to test server's remote repo
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://$PUSH_USER@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # start updated services
-ssh apps@$IP -p $PORT sudo -iu apps <<EOF
+ssh $RUN_USER@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  sudo service docker restart # restart docker service to prevent "timeout" errors (https://github.com/docker/compose/issues/3633#issuecomment-254194717)
-  sudo make run-docker
+  service docker restart # restart docker service to prevent "timeout" errors (https://github.com/docker/compose/issues/3633#issuecomment-254194717)
+  make run-docker
   # run migrations
   docker exec backend sh -c "cd /usr/local/go/src/github.com/AlexSugak/getsky-trade/db/ && bash ./migrate.sh"
 EOF
