5f1ba7275e35e5b7aad4310807d0f7625f78d7b7
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-03-16 12:46:45 +0200

    #9 Add env variable to 'environment' field

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -24,8 +24,7 @@ git push deploy master
 ssh $RUN_USER@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   sudo service docker restart # restart docker service to prevent "timeout" errors (https://github.com/docker/compose/issues/3633#issuecomment-254194717)
-  echo key ${RECAPTCHA_SECRET}
-  sudo -E make run-docker
+  make run-docker
   # run migrations
   docker exec backend sh -c "cd /usr/local/go/src/github.com/AlexSugak/getsky-trade/db/ && bash ./migrate.sh"
 EOF
