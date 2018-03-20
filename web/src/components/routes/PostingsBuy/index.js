f210afbed994c728520a00a02b3506f7130a20cb
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-03-20 12:29:49 +0200

    #16 Add navigation to posting advert

---
+++ web/src/components/routes/PostingsBuy/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostingsBuy = (props) => {
+    return(
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default PostingsBuy
--- web/src/components/routes/Routes.js
+++ web/src/components/routes/Routes.js
@@ -6,6 +6,7 @@ import LatestAdverts from './LatestAdverts';
 import SearchAdverts from './SearchAdverts';
 import Registration from './Registration';
 import Login from './Login';
+import PostingsBuy from './PostingsBuy';
 
 const Routes = ({ match }) => {
     return (
@@ -13,6 +14,7 @@ const Routes = ({ match }) => {
             <Route path={`/search`} component={SearchAdverts} />
             <Route path={`/register`} component={Registration} />
             <Route path={`/login`} component={Login} />
+            <Route path={`/postings/buy`} component={PostingsBuy} />
             <Route path={`/`} component={LatestAdverts} />
         </Switch>
     );
