03c6cce0599319877f6a35d685ab83d0e11e9240
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-04-04 16:59:02 +0300

    Update dashboard reducer to process message amounts

--- web/src/components/routes/Dashboard/reducers.js
+++ web/src/components/routes/Dashboard/reducers.js
@@ -14,12 +14,15 @@ export default (state = initialState, action) => {
         case ADVERTS_DASHBOARD_RESPONSE:
             const { myAdverts, enquiredAdverts } = action;
 
-            const buyAdverts = myAdverts.filter(a => a.type === ADVERT_BUY);
-            const sellAdverts = myAdverts.filter(a => a.type === ADVERT_SELL);
-            const buyEnquiries = enquiredAdverts.filter(a => a.type === ADVERT_BUY);
-            const sellEnquiries = enquiredAdverts.filter(a => a.type === ADVERT_SELL);
+            const myAdvertsNewMessages = myAdverts.reduce((acc, cur) => cur.newMessagesAmount + acc, 0)
+            const enquiredAdvertsNewMessages = enquiredAdverts.reduce((acc, cur) => cur.newMessagesAmount + acc, 0)
 
-            return { ...state, buyAdverts, sellAdverts, buyEnquiries, sellEnquiries };
+            const buyAdverts = myAdverts.filter(a => a.advert.type === ADVERT_BUY);
+            const sellAdverts = myAdverts.filter(a => a.advert.type === ADVERT_SELL);
+            const buyEnquiries = enquiredAdverts.filter(a => a.advert.type === ADVERT_BUY);
+            const sellEnquiries = enquiredAdverts.filter(a => a.advert.type === ADVERT_SELL);
+
+            return { ...state, buyAdverts, sellAdverts, buyEnquiries, sellEnquiries, newMessages: myAdvertsNewMessages + enquiredAdvertsNewMessages };
         default:
             return state;
     }
