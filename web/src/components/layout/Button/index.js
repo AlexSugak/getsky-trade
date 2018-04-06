afe7e9402853beb47eb59c25ac6924a6942f6b49
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-04-06 14:43:18 +0300

    Add action button component

--- web/src/components/layout/Button/index.js
+++ web/src/components/layout/Button/index.js
@@ -1,5 +1,6 @@
 import BuyButton from './BuyButton';
 import SellButton from './SellButton';
 import Button from './Button';
+import ActionButton from './ActionButton';
 
-export { BuyButton, SellButton, Button };
+export { BuyButton, SellButton, Button, ActionButton };
