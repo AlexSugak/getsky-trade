e5b11de6abcd2aa428906b45d2f5820b0f981ec8
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-04-11 10:47:17 +0300

    Add svg logo

--- web/src/components/layout/Brand/Brand.js
+++ web/src/components/layout/Brand/Brand.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const WhiteText = styled.span`
-    color: ${props => props.theme.colors.white};
-    font-size: ${props => props.theme.logoSize}px;
-`
-
-const BlueText = styled.span`
-    color: ${props => props.theme.colors.blue};
-    font-size: ${props => props.theme.logoSize}px;
-`
+import logo from './logo_getsky.svg';
 
 const Brand = styled(Link) `
     display: block;
-    transform: scale(1, 1.25);
-    -webkit-transform: scale(1, 1.25); /* Safari and Chrome */
-    -moz-transform: scale(1, 1.25); /* Firefox */
-    -ms-transform: scale(1, 1.25); /* IE 9+ */
-    -o-transform: scale(1, 1.25); /* Opera */
-    letter-spacing: 3px;
 `;
 
 export default () => (
     <Brand to="/">
-        <WhiteText>BUY</WhiteText>
-        <BlueText>SKY</BlueText>
+        <img src={logo} />>
     </Brand>
 );
