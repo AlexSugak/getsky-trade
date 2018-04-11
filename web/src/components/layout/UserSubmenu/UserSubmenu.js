7c28dca3638faac4af42bc8ed4b58c5f9a9c9f7d
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-04-11 12:35:29 +0300

    Redesign: TopBar menu

--- web/src/components/layout/UserSubmenu/UserSubmenu.js
+++ web/src/components/layout/UserSubmenu/UserSubmenu.js
@@ -1,54 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Flex, Box } from 'grid-styled';
 import { Link } from 'react-router-dom';
 
 import Expander from 'components/layout/Expander';
 import theme from 'components/theme';
+
+import icDashboard from './icons/icDashboard.svg';
+import icLogout from './icons/icLogout.svg';
+import icSettings from './icons/icSettings.svg';
+
+const Ic = styled.img`
+    width: 16px;
+    height: 16px;
+    margin-right: 14px;
+`
 
 const UserName = styled.span`
     color: ${theme.colors.white};
     font-size: ${theme.fontSizes[0]}px;
 `;
 
-const UserMenu = styled.ul`
+const UserMenu = styled(Box) `
     position: absolute;
-    list-style: none;
-    background-color: ${theme.colors.black};
+    z-index: 1000;
+    margin-top: 11px;
+    padding: 8px 0px;
+    background-color: ${theme.colors.darkBlue};
 `;
 
-const UserMenuItem = styled.li`
-    margin-bottom: -1px;
-    & > * {
-        padding: 10px 50px;
-        display: block;
-    }
-    border-top: 1px solid ${theme.colors.lightGray};
-    border-bottom: 1px solid ${theme.colors.lightGray};
+const UserMenuItem = styled(Flex) `
+    padding: 8px 23px;
+    font-size: ${theme.fontSizes[1]}px;
     color: ${theme.colors.white};
 
-
     &:hover {
-        opacity: 0.5;
         cursor: pointer;
-        background-color: ${theme.colors.white};
-
-        font-weight: bold;
-        color: ${theme.colors.black};
-    }
-
-    &:last-child {
-        border-top: 0;
-        border-bottom: 0;
+        color: ${theme.colors.blue};
     }
 `;
 
 const NavLink = styled(Link) `
+    width: 100%;
     color: ${theme.colors.white};
-
-    ${UserMenuItem}:hover & {
-        font-weight: bold;
-        color: ${theme.colors.black};
+    &:hover {
+        color: ${theme.colors.blue};
     }
 `;
 
@@ -59,13 +56,16 @@ const UserSubmenu = ({ userInfo, logout }) => (
         </UserName>
     }>
         <UserMenu>
-            <UserMenuItem>
+            <UserMenuItem alignItems={'center'}>
+                <Ic src={icDashboard} />
                 <NavLink to="/dashboard">Dashboard</NavLink>
             </UserMenuItem>
-            <UserMenuItem>
+            <UserMenuItem alignItems={'center'}>
+                <Ic src={icSettings} />
                 <NavLink to="/user-settings">Settings</NavLink>
             </UserMenuItem>
-            <UserMenuItem onClick={logout}>
+            <UserMenuItem onClick={logout} alignItems={'center'}>
+                <Ic src={icLogout} />
                 <span>Log out</span>
             </UserMenuItem>
         </UserMenu>
